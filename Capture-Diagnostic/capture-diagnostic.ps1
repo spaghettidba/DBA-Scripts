58e283cde4a959ab99a7bef5b96346066baf8d2d
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2019-08-14 13:05:53 +0200

    Fixed errors when adding server and database columns

--- Capture-Diagnostic/capture-diagnostic.ps1
+++ Capture-Diagnostic/capture-diagnostic.ps1
@@ -11,7 +11,6 @@ $queries = @(
     "Overall Index Usage - Reads",
     "Overall Index Usage - Writes",
     "Drive Level Latency",
-    "Connection Counts by IP Address"
     "File Sizes and Space",
     "Log Space Usage",
     "IO Stats By File",
@@ -30,30 +29,48 @@ Invoke-DbaDiagnosticQuery -SqlInstance "localhost\sqlexpress2016"  -QueryName $q
     ForEach-Object {
         $TableName = $_.Name
         $DatabaseName = $_.Database
+        $ServerName = $_.SqlInstance
 
         $snapshotProp = @{
             Label = "snapshot_id"
             Expression = {$SnapshotId}
         }
+        $serverProp = @{
+            Label = "Server Name"
+            Expression = {$ServerName}
+        }
+        $databaseProp = @{
+            Label = "Database Name"
+            Expression = {$DatabaseName}
+        }
 
-        $expr = '$_.Result | Select-Object $snapshotProp, *'
+        $expr = '$_.Result | Select-Object $snapshotProp, '
 
-        # Decide whether $Data needs an additional "database" column
-
-        if($_.DatabaseSpecific) {
-            $databaseProp = @{
-                Label = "Database Name"
-                Expression = {$DatabaseName}
-            }
-            if((Get-Member -InputObject $_.Result -MemberType NoteProperty | Where-Object { $_.Name -eq "Database Name" } | Measure-Object) -eq 0) {
-                $expr = '$_.Result | Select-Object $snapshotProp, $databaseProp, *'
+        # Decide whether collection needs an additional "server" column
+        # Two different checks are required, because the input collection
+        # could contain objects of different types (System.Data.DataRow or PsCustomObject)
+        if(-not (($_.Result.PSObject.Properties | Select-Object -Expand Name) -contains "Server Name")) {
+            if(($_.Result | Get-Member -MemberType NoteProperty -Name "Server Name" | Measure-Object).Count -eq 0) {
+                $expr += ' $serverProp, '
             }
         }
+
+        # Decide whether collection needs an additional "database" column
+        # again, two different checks
+        if($_.DatabaseSpecific) {
+            if(-not (($_.Result.PSObject.Properties | Select-Object -Expand Name) -contains "Database Name")) {
+                if(($_.Result | Get-Member -MemberType NoteProperty -Name "Database Name" | Measure-Object).Count -eq 0) {
+                    $expr += ' $databaseProp, '
+                }
+            }
+        }
+
+        $expr += '*'
 
         $param = @{
             SqlInstance     = "localhost\sqlexpress2016"
             Database        = "testdiagnostic"
-            Schema          = "staging"
+            Schema          = "dbo"
             AutoCreateTable = $true
             Table           = $TableName
             InputObject     = Invoke-Expression $expr
