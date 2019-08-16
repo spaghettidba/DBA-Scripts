3c99fa9c7fbe9c0030fb3b1219cf508d16da494e
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2019-08-16 11:15:17 +0200

    Added code to store plans in a separate table

--- Capture-Diagnostic/Capture-WhoIsActive.ps1
+++ Capture-Diagnostic/Capture-WhoIsActive.ps1
@@ -37,7 +37,51 @@ $param = @{
     Table           = "WhoIsActive"
 }
 
-Invoke-DbaWhoIsActive -SqlInstance $SourceServer -FindBlockLeaders -GetOuterCommand -GetPlans 1 -ShowOwnSpid |
+Invoke-DbaWhoIsActive -SqlInstance $SourceServer -FindBlockLeaders -GetOuterCommand -GetPlans 1 |
     Where-Object {$_ -ne $null } | 
     Select-Object $snapshotProp, $serverProp, * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors |
     Write-DbaDataTable @param 
+
+
+$sql = "
+IF NOT EXISTS (
+	SELECT *
+	FROM sys.columns
+	WHERE object_id = OBJECT_ID('WhoIsActive')
+		AND name = 'plan_hash'
+)
+ALTER TABLE [dbo].[WhoIsActive] ADD plan_hash binary(16);
+
+
+UPDATE [dbo].[WhoIsActive] 
+SET plan_hash = master.sys.fn_repl_hash_binary(CONVERT(varbinary(max),query_plan))
+WHERE query_plan IS NOT NULL;
+
+
+IF OBJECT_ID('WhoIsActive_plans') IS NULL
+BEGIN
+	CREATE TABLE [dbo].[WhoIsActive_plans](
+		[plan_hash] [binary](16) PRIMARY KEY,
+		[query_plan] [nvarchar](max) NULL
+	)
+END
+
+
+MERGE INTO [dbo].[WhoIsActive_plans] AS trg
+USING (
+	SELECT DISTINCT plan_hash, query_plan
+	FROM [WhoIsActive]
+	WHERE query_plan IS NOT NULL
+) AS src
+	ON src.plan_hash = trg.plan_hash
+WHEN NOT MATCHED THEN INSERT (plan_hash, query_plan)
+VALUES (src.plan_hash, src.query_plan);
+
+
+UPDATE [dbo].[WhoIsActive] 
+SET query_plan = NULL
+WHERE query_plan IS NOT NULL
+	AND plan_hash IS NOT NULL;
+"
+
+Invoke-DbaQuery -SqlInstance $DestinationServer -Database $DestinationDatabase -Query $sql
