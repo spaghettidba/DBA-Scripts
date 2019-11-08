f0dd76ee2fc9fbb1e7274a62c5cb7db4d721ff9e
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2019-11-08 18:06:43 +0100

    Update remove-expiredBackups.ps1

--- Backup-Database/remove-expiredBackups.ps1
+++ Backup-Database/remove-expiredBackups.ps1
@@ -5,4 +5,8 @@ param (
     [Parameter(Mandatory)]
     [int] $RetentionDays
 )
-Get-ChildItem -Path $RootBackupFolder -Recurse -File | Where-Object {$PSItem.CreationTime -lt (get-date).AddDays($RetentionDays) } | Remove-Item
+
+get-childitem -Path $RootBackupFolder -Recurse -File | Where-Object {$PSItem.CreationTime -lt (get-date).AddDays(-$RetentionDays) } | Remove-Item
+
+
+
